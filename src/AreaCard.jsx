0e5d9e4e90fd451e4af737c7f09bde63775bd20a
Damog83 <damiengittings@hotmail.co.uk>
2023-01-09 12:45:52 +0000

    branch

--- src/AreaCard.jsx
+++ src/AreaCard.jsx
@@ -1,38 +1,44 @@
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
+import {
+	Card,
+	CardHeader,
+	CardMedia,
+	CardContent,
+	Avatar,
+	Typography,
+} from '@mui/material';
 import { Box } from '@mui/system';
-import blue from '@mui/material/colors/blue';
+import { blue } from '@mui/material/colors';
 
 export default function AreaCard({ area }) {
 	return (
 		<Box m={2}>
-		<Card sx={{ maxWidth: 345 }}>
-			<CardHeader
-				avatar={<Avatar sx={{ bgcolor: blue[500] }}  >{`${area['state abbreviation']}`}</Avatar>}
-				title={`${area['place name']}`}
-			/>
-			  <CardMedia
-        component="img"
-        height="100"
-        image="/src/post.jpg"
-        alt="Image of an envelope with the gps icon lay over the top"
-      />
-			<CardContent>
-				<Typography variant="body2" color="text.secondary">
-					Latitude: {`${area.latitude}`}
-				</Typography>
-				<Typography variant="body2" color="text.secondary">
-					Longitude: {`${area.longitude}`}
-				</Typography>
-				<Typography variant="body2" color="text.secondary">
-					State:{`${area.state}`}
-				</Typography>
-			</CardContent>
-		</Card>
+			<Card sx={{ maxWidth: 345 }}>
+				<CardHeader
+					avatar={
+						<Avatar
+							sx={{ bgcolor: blue[500] }}
+						>{`${area['state abbreviation']}`}</Avatar>
+					}
+					title={`${area['place name']}`}
+				/>
+				<CardMedia
+					component="img"
+					height="100"
+					image="/src/post.jpg"
+					alt="Image of an envelope with the gps icon lay over the top"
+				/>
+				<CardContent>
+					<Typography variant="body2" color="text.secondary">
+						Latitude: {`${area.latitude}`}
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						Longitude: {`${area.longitude}`}
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						State:{`${area.state}`}
+					</Typography>
+				</CardContent>
+			</Card>
 		</Box>
 	);
 }
